72fe7564f83363ac4e075272d3e67ee6fc4dbf81
David Cámara Iglesias <david.c.iglesias@gmail.com>
2018-10-16 22:26:24 +0200

    Updates

---
+++ precache-manifest.bad3e59aa38c4da773fb8d3bc173204c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6b434c199516bc61b48c6c5cc4c34aa",
+    "url": "/sweet-winter/static/media/winter.c6b434c1.svg"
+  },
+  {
+    "revision": "fa4f01fe11848fb7df6c",
+    "url": "/sweet-winter/static/js/runtime~main.fa4f01fe.js"
+  },
+  {
+    "revision": "3b3f4e7eedbbb977d4b0",
+    "url": "/sweet-winter/static/js/main.3b3f4e7e.chunk.js"
+  },
+  {
+    "revision": "93574c426126d31c48b2",
+    "url": "/sweet-winter/static/js/1.93574c42.chunk.js"
+  },
+  {
+    "revision": "3b3f4e7eedbbb977d4b0",
+    "url": "/sweet-winter/static/css/main.728fe27c.chunk.css"
+  },
+  {
+    "revision": "989525af95acf761503a251a0968a673",
+    "url": "/sweet-winter/index.html"
+  }
+];
